4766fcc3fa6854607099943a0853b41c492011d7
licheng1013 <2501093733@qq.com>
2022-10-22 18:07:34 +0800

    增加列表，hashMap演示！

--- src/test/test.rs
+++ src/test/test.rs
@@ -4,9 +4,23 @@
 //     V2,
 // }
 
+
+use std::collections::HashMap;
+
 /// 其他demo
 pub fn test_study() {
+    println!("开始练习-----------------------------------------------");
     let mut  v: Vec<i32> = Vec::new();
     v.push(1);
     v.push(2);
+    println!("{}",v[0]);
+
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    let score = scores.get(&String::from("Blue"));
+    println!("{:?}",score);
+
+    println!("练习结束-----------------------------------------------");
 }
