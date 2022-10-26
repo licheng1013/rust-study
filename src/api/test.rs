692b90ff1471c1f8809199ced4a4ad46d65a898f
licheng1013 <2501093733@qq.com>
2022-10-26 18:46:46 +0800

    注释无法使用的代码！

--- src/api/test.rs
+++ src/api/test.rs
@@ -1,8 +1,9 @@
-use crate::service::user::UserService;
-use crate::util::r;
+use actix_web::{Responder, Result};
 use actix_web::get;
 use actix_web::web::{Json, Path};
-use actix_web::{Responder, Result};
+
+use crate::service::user::UserService;
+use crate::util::r;
 
 /// 测试错误返回
 #[get("/1")]
@@ -23,3 +24,10 @@ pub async fn test2() -> Result<impl Responder> {
 pub async fn test3(list: Path<String>) -> Result<impl Responder> {
     Ok(Json(r::ok_data(list.into_inner())))
 }
+
+// /// 文件上传
+// #[get("/4/")]
+// pub async fn test4(file: Form<Stdout>) -> Result<impl Responder> {
+//
+//     Ok(Json(r::ok_data("上传文件成功！")))
+// }
