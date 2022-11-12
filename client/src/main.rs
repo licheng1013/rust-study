7cea76adce51697e0797dc6dc7792b81360fecb6
licheng1013 <2501093733@qq.com>
2022-11-12 10:00:26 +0800

    优化代码！

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,17 +1,31 @@
 
-use std::io::{self, prelude::*, BufReader};
+use std::io::{self, prelude::*, BufReader, BufWriter};
 use std::net::TcpStream;
 use std::str;
+use std::time::Duration;
 
 fn main() -> io::Result<()> {
+    // let stream = TcpStream::connect("127.0.0.1:8080")?;
+    // //创建变量stream，直接连接sever端
+    // loop {
+    //     let mut reader = BufReader::new(&stream);
+    //     //从stream流创建一个读，目的是要从我们的server端读，
+    //     let mut buffer: Vec<u8> = Vec::new();
+    //     reader.read_until(b'\n', &mut buffer)?;
+    //     //一直读到换行为止（b'\n'中的b表示字节），读到buffer里面
+    //     println!("服务端数据: {}", str::from_utf8(&buffer).unwrap());
+    // }
+    client2()
+}
+
+fn client2() -> io::Result<()> {
     let stream = TcpStream::connect("127.0.0.1:8080")?;
     //创建变量stream，直接连接sever端
     loop {
-        let mut reader = BufReader::new(&stream);
-        //从stream流创建一个读，目的是要从我们的server端读，
-        let mut buffer: Vec<u8> = Vec::new();
-        reader.read_until(b'\n', &mut buffer)?;
-        //一直读到换行为止（b'\n'中的b表示字节），读到buffer里面
-        println!("服务端数据: {}", str::from_utf8(&buffer).unwrap());
+        //let mut reader = BufReader::new(&stream);
+        let mut writer = BufWriter::new(&stream);
+        writer.write("HellWorld".as_ref()).unwrap();
+        // 休眠一秒
+        std::thread::sleep( Duration::from_secs(1));
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,26 @@
-use std::net::{TcpListener, TcpStream};
-use std::thread;
+use std::net::{Shutdown, TcpListener, TcpStream};
+
 //std::thread库的引入，对输入的每一个流创建一个线程
-use std::time;
+
 use std::io::{self, BufRead, BufReader, Read, Write};
 use std::str;
+
 //引入io库，为了处理错误
 
-fn handle_client(stream: TcpStream) -> io::Result<()> {
+fn handle_server(mut stream: TcpStream) -> io::Result<()> {
     loop {
         let mut reader = BufReader::new(&stream);
-        let mut message = "".to_string();
-        reader.read_to_string( &mut message)?;
-        println!("服务端数据: {}", message);
-        //stream.write("HelloWorld\n".as_ref())?;
-        //否则把它写回去
-        //thread::sleep(time::Duration::from_secs(1));
-        //调用sleep函数实现服务的间隔，间隔1s
+        //let mut message = "".to_string();
+        let mut message = vec![0; 1024 * 8];
+        let i = reader.read(&mut message)?;
+        if i > 0 {
+            println!(
+                "服务端数据: {}",
+                String::from_utf8((&message[0..i]).to_vec()).unwrap()
+            );
+            //stream.write("HelloWorld".as_ref()).unwrap();
+            //stream.shutdown(Shutdown::Both).unwrap()
+        }
     }
 }
 
@@ -25,8 +30,7 @@ fn main() -> io::Result<()> {
     //创建一个容器，用来放线程的句柄
     for stream in listener.incoming() {
         println!("建立连接");
-        handle_client(stream?)?;
+        handle_server(stream?)?;
     }
     Ok(())
 }
-
