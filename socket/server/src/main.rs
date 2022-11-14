267eeb86652209d035e710fd13a2adb2cfa4973c
licheng1013 <2501093733@qq.com>
2022-11-14 18:47:44 +0800

    优化测试功能！

--- socket/server/src/main.rs
+++ socket/server/src/main.rs
@@ -1,16 +1,16 @@
-use std::net::{TcpListener, TcpStream};
+use std::net::{Shutdown, TcpListener, TcpStream};
 
 //std::thread库的引入，对输入的每一个流创建一个线程
 
-use std::io::{self, BufReader, Read};
+use std::io::{self, BufReader, BufWriter, Read, Write};
 use std::thread;
-
 
 //引入io库，为了处理错误
 
 fn handle_server(stream: TcpStream) -> io::Result<()> {
     loop {
         let mut reader = BufReader::new(&stream);
+        let mut writer = BufWriter::new(&stream);
         //let mut message = "".to_string();
         let mut message = vec![0; 1024 * 8];
         let i = reader.read(&mut message)?;
@@ -21,6 +21,10 @@ fn handle_server(stream: TcpStream) -> io::Result<()> {
             );
             //stream.write("HelloWorld".as_ref()).unwrap();
             //stream.shutdown(Shutdown::Both).unwrap()
+
+            writer.write(&message[0..i]).unwrap();
+            writer.flush().unwrap();
+            stream.shutdown(Shutdown::Both).unwrap();
         }
     }
 }
@@ -30,8 +34,8 @@ fn main() -> io::Result<()> {
     //定义一个listener，bind函数里面填写的是监听的的ip与端口号,?是一种简写，等价于except,unwrap
     //创建一个容器，用来放线程的句柄
     for stream in listener.incoming() {
-        println!("建立连接");
-        thread::spawn(  ||  {
+        thread::spawn(|| {
+            println!("建立连接");
             handle_server(stream.unwrap()).unwrap();
         });
     }
