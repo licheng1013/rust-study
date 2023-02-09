fb4d13e7df8def848614d70e84ef3a9f29f0624d
licheng1013 <2501093733@qq.com>
2023-02-09 15:51:09 +0800

    优化文档!

--- rbatis/src/main.rs
+++ rbatis/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use dotenvy::dotenv;
 use rbatis::Rbatis;
 use rbatis::rbdc::datetime::FastDateTime;
+use rbatis::rbdc::uuid::Uuid;
 use rbatis::sql::PageRequest;
 use rbdc_mysql::driver::MysqlDriver;
 use crate::service::admin::Admin;
@@ -13,43 +14,39 @@ pub async fn main() {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("没有设置数据库连接");
 
-    // enable log crate to show sql logs
-    fast_log::init(fast_log::Config::new().console()).expect("初始化日志失败");
-    // initialize rbatis. also you can call rb.clone(). this is  an Arc point
+    // 日志开启
+    //fast_log::init(fast_log::Config::new().console()).expect("初始化日志失败");
+    // 初始话联机对象
     let mut rb = Rbatis::new();
     //mysql
     rb.init(MysqlDriver{},database_url.as_str()).unwrap();
 
 
-    let activity = Admin {
-        admin_id: Some(2.into()),
+    let mut activity = Admin {
+        admin_id: Some(1.into()),
         user_name: Some("用户名称".into()),
         password:  Some("密码".into()),
         salt:  Some("盐值".into()),
         create_time:  Some(FastDateTime::now()),
         nick_name: Some("昵称".into()),
     };
-    let data = Admin::insert(&mut rb, &activity).await;
-    println!("insert = {:?}", data);
+    // let data = Admin::insert(&mut rb, &activity).await;
+    // println!("insert = {:?}", data);
 
-    let data = Admin::select_all_by_id(&mut rb, "1").await;
-    println!("select_all_by_id = {:?}", data);
+    let data = Admin::list(&mut rb, "1").await;
+    println!("查询多条记录 = {:?}", data);
 
-    let data = Admin::select_by_id(&mut rb, "1".to_string()).await;
-    println!("select_by_id = {:?}", data);
+    let data = Admin::one(&mut rb, "1").await;
+    println!("查询单条记录 = {:?}", data);
 
-    let data = Admin::update_by_column(&mut rb, &activity, "id").await;
-    println!("update_by_column = {:?}", data);
+    activity.user_name = Some(Uuid::new().to_string()[0..7].to_string());
+    let data = Admin::update(&mut rb, &activity, "1").await;
+    println!("修改 = {:?}", data);
 
-    let data = Admin::update_by_name(&mut rb, &activity, "test").await;
-    println!("update_by_name = {:?}", data);
+    // let data = Admin::delete(&mut rb, "2").await;
+    // println!("删除 = {:?}", data);
 
-    // let data = Admin::delete_by_column(&mut rb, "id", &"2".into()).await;
-    // println!("delete_by_column = {:?}", data);
 
-    let data = Admin::delete_by_name(&mut rb, "2").await;
-    println!("delete_by_column = {:?}", data);
-
-    let data = Admin::select_page(&mut rb, &PageRequest::new(1, 10), "2").await;
-    println!("select_page = {:?}", data);
+    let data = Admin::page(&mut rb, &PageRequest::new(1, 10), "2").await;
+    println!("分页 = {:?}", data);
 }
--- rbatis/src/service/admin.rs
+++ rbatis/src/service/admin.rs
@@ -17,10 +17,8 @@ pub struct  Admin {
 
 rbatis::crud!(Admin {},TABLE_NAME);
 
-impl_select!(Admin{select_all_by_id(id:&str) => "`where admin_id = #{id}`"},TABLE_NAME);
-impl_select!(Admin{select_by_id(id:String) -> Option => "`where id = #{id} limit 1`"},TABLE_NAME);
-impl_update!(Admin{update_by_name(name:&str) => "`where id = 1`"},TABLE_NAME);
-impl_delete!(Admin {delete_by_name(name:&str) => "`where name= '2'`"},TABLE_NAME);
-//limiting condition：maybe pg/mssql not support sql `limit 0,10` you should add arg  `limit_sql:&str` of set limit_sql = " limit 0 offset 10"
-//`impl_select_page!(BizActivity{select_page(name:&str,limit_sql:&str) => "`where name != #{name}`"});`
-impl_select_page!(Admin{select_page(name:&str) => "`where name != #{name}`"},TABLE_NAME);
+impl_select!(Admin{list(id:&str) => "`where admin_id = #{id}`"},TABLE_NAME);
+impl_select!(Admin{one(id:&str) -> Option => "`where admin_id = #{id} limit 1`"},TABLE_NAME);
+impl_update!(Admin{update(id:&str) => "`where admin_id = #{id}`"},TABLE_NAME);
+impl_delete!(Admin {delete(id:&str) => "`where admin_id = #{id}`"},TABLE_NAME);
+impl_select_page!(Admin{page(name:&str) => "`where user_name = #{name}`"},TABLE_NAME);
