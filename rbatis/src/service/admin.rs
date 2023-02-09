71056213060c3228c06defa66e7aaec0c1159407
licheng1013 <2501093733@qq.com>
2023-02-09 14:53:21 +0800

    优化 rbatis 示例!

--- rbatis/src/service/admin.rs
+++ rbatis/src/service/admin.rs
@@ -1,7 +1,9 @@
+use std::string::ToString;
 use rbatis::rbdc::datetime::FastDateTime;
 use serde::{Deserialize, Serialize};
 use rbatis::{impl_update, impl_delete, impl_select_page, impl_select};
 
+const TABLE_NAME:&str = "t_admin";
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct  Admin {
@@ -13,12 +15,12 @@ pub struct  Admin {
     pub nick_name: Option<String>,
 }
 
-rbatis::crud!(Admin {},"t_admin");
+rbatis::crud!(Admin {},TABLE_NAME);
 
-impl_select!(Admin{select_all_by_id(id:&str) => "`where admin_id = #{id}`"},"t_admin");
-impl_select!(Admin{select_by_id(id:String) -> Option => "`where id = #{id} limit 1`"},"t_admin");
-impl_update!(Admin{update_by_name(name:&str) => "`where id = 1`"},"t_admin");
-impl_delete!(Admin {delete_by_name(name:&str) => "`where name= '2'`"},"t_admin");
+impl_select!(Admin{select_all_by_id(id:&str) => "`where admin_id = #{id}`"},TABLE_NAME);
+impl_select!(Admin{select_by_id(id:String) -> Option => "`where id = #{id} limit 1`"},TABLE_NAME);
+impl_update!(Admin{update_by_name(name:&str) => "`where id = 1`"},TABLE_NAME);
+impl_delete!(Admin {delete_by_name(name:&str) => "`where name= '2'`"},TABLE_NAME);
 //limiting condition：maybe pg/mssql not support sql `limit 0,10` you should add arg  `limit_sql:&str` of set limit_sql = " limit 0 offset 10"
 //`impl_select_page!(BizActivity{select_page(name:&str,limit_sql:&str) => "`where name != #{name}`"});`
-impl_select_page!(Admin{select_page(name:&str) => "`where name != #{name}`"},"t_admin");
+impl_select_page!(Admin{select_page(name:&str) => "`where name != #{name}`"},TABLE_NAME);
