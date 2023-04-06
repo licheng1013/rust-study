b7a980ccaa04d6fa60ba46b0c541e66ed5bedc5b
licheng1013 <2501093733@qq.com>
2023-04-06 09:54:48 +0800

    重新格式化

--- study/src/list.rs
+++ study/src/list.rs
@@ -8,12 +8,12 @@ mod test {
         let mut v: Vec<i32> = Vec::new();
         // 增加
         v.push(1);
-        v.push(2); 
+        v.push(2);
         //删除
-        v.remove(1); 
+        v.remove(1);
         //插入
-        v.insert(0, 3); 
+        v.insert(0, 3);
         println!("获取: {:?},长度: {:?},列表: {:?}", v.get(0), v.len(), v);
-        println!("切片: {:?}",&v[0..v.len()]);
+        println!("切片: {:?}", &v[0..v.len()]);
     }
 }
