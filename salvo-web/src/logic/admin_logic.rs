9f3cd533521b8b89863216399226e7f2411acf6e
licheng <2501093733@qq.com>
2023-08-03 11:27:46 +0800

    增删改生成完毕！

--- salvo-web/src/logic/admin_logic.rs
+++ salvo-web/src/logic/admin_logic.rs
@@ -1,6 +1,9 @@
+use std::future::IntoFuture;
 use rbatis::{crud, impl_delete, impl_select, impl_select_page, impl_update};
 use rbatis::rbdc::datetime::DateTime;
 use rbatis::sql::PageRequest;
+use rbs::Value;
+use rbs::value::map::ValueMap;
 use crate::model::admin::Admin;
 use crate::RB;
 use crate::util::page::{PageParam, PageResult};
@@ -8,17 +11,15 @@ use crate::util::page::{PageParam, PageResult};
 const TABLE_NAME: &str = "t_admin";
 
 crud!(Admin{},TABLE_NAME);
-
-impl_select!(Admin{select_by_id(id:String) -> Option => "`where id = #{id} limit 1`"},TABLE_NAME);
-impl_update!(Admin{update(id:&str) => "`where id = #{id}`"},TABLE_NAME);
-impl_delete!(Admin {delete(id:&str) => "`where id = #{id}`"},TABLE_NAME);
-impl_select_page!(Admin{page(name:&str) => "``"},TABLE_NAME);
+impl_select_page!(Admin{page(where_str:&str) => "${where_str}"},TABLE_NAME);
 
 pub async fn list(page: PageParam, admin: Admin) -> PageResult<Vec<Admin>> {
+    let condition = where_condition(admin);
+    println!("{condition:?}");
     let result = Admin::page(
         &mut RB.clone(),
         &PageRequest::new(page.page.unwrap_or(1)
-                          , page.size.unwrap_or(10)), "").await.unwrap();
+                          , page.size.unwrap_or(10)), &condition).await.unwrap();
     return PageResult {
         total: result.total,
         list: result.records,
@@ -41,6 +42,33 @@ pub async fn insert(mut admin: Admin) {
     admin.create_time = Some(DateTime::now());
     let result = Admin::insert(&mut RB.clone(), &admin).await.unwrap();
     println!("{result:?}")
+}
+
+pub fn where_condition(admin: Admin) -> String {
+    let mut where_str = String::from("");
+    if admin.id != None {
+        where_str.push_str(format!("and id = \"{}\"", admin.id.unwrap()).as_str())
+    }
+    if admin.nick_name != None {
+        where_str.push_str(format!("and nick_name = \"{}\"", admin.nick_name.unwrap()).as_str())
+    }
+    if admin.user_name != None {
+        where_str.push_str(format!("and user_name = \"{}\"", admin.user_name.unwrap()).as_str())
+    }
+    if admin.salt != None {
+        where_str.push_str(format!("and salt = \"{}\"", admin.salt.unwrap()).as_str())
+    }
+    if admin.password != None {
+        where_str.push_str(format!("and password = \"{}\"", admin.password.unwrap()).as_str())
+    }
+    if admin.create_time != None {
+        where_str.push_str(format!("and create_time = \"{}\"", admin.create_time.unwrap()).as_str())
+    }
+    if where_str.len() == 0 {
+        return "".to_string();
+    }
+    where_str = where_str[3..where_str.len()].to_string();
+    return format!("where{}",where_str)
 }
 
 
