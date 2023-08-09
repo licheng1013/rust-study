78e3880c28e04ce50f3035c830ce719b11d890c8
licheng <2501093733@qq.com>
2023-08-09 10:43:28 +0800

    调整描述功能!

--- salvo-web/src/api/admin_api.rs
+++ salvo-web/src/api/admin_api.rs
@@ -1,9 +1,8 @@
-use rbatis::sql::PageRequest;
 use salvo::prelude::*;
 use crate::logic;
 use crate::model::admin::Admin;
 use crate::util::page::PageParam;
-use crate::util::result::ok_data;
+use crate::util::result::{ok_data, ok_msg};
 
 #[handler]
 async fn list(_req: &mut Request, res: &mut Response) {
@@ -17,21 +16,21 @@ async fn list(_req: &mut Request, res: &mut Response) {
 async fn update(_req: &mut Request, res: &mut Response) {
     let model: Admin = _req.parse_json().await.unwrap();
     logic::admin_logic::update(model).await;
-    res.render(Json(ok_data("修改成功!")));
+    res.render(Json(ok_msg("修改成功!".to_string())));
 }
 
 #[handler]
 async fn delete(_req: &mut Request, res: &mut Response) {
     let model: Admin = _req.parse_json().await.unwrap();
     logic::admin_logic::delete(model).await;
-    res.render(Json(ok_data("删除成功!")));
+    res.render(Json(ok_msg("删除成功!".to_string())));
 }
 
 #[handler]
 async fn insert(_req: &mut Request, res: &mut Response) {
     let model: Admin = _req.parse_json().await.unwrap();
     logic::admin_logic::insert(model).await;
-    res.render(Json(ok_data("插入成功!")));
+    res.render(Json(ok_msg("插入成功!".to_string())));
 }
 
 pub fn router() -> Router {
