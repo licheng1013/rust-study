3723d430b9745c2f653c704ef25e9dc0d0fbe1bb
licheng <2501093733@qq.com>
2023-09-05 17:55:09 +0800

    fix RBatis 示例!

--- rbatis/src/main.rs
+++ rbatis/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use dotenvy::dotenv;
-use rbatis::Rbatis;
-use rbatis::rbdc::datetime::FastDateTime;
+use rbatis::RBatis;
+use rbatis::rbdc::datetime::DateTime;
 use rbatis::rbdc::uuid::Uuid;
 use rbatis::sql::PageRequest;
 use rbdc_mysql::driver::MysqlDriver;
@@ -12,41 +12,37 @@ mod service;
 #[tokio::main]
 pub async fn main() {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("没有设置数据库连接");
+    let database_url = "mysql://root:root@192.168.101.90:3306/t_gorm".to_string();
 
     // 日志开启
     //fast_log::init(fast_log::Config::new().console()).expect("初始化日志失败");
     // 初始话联机对象
-    let mut rb = Rbatis::new();
+    let mut rb = RBatis::new();
     //mysql
     rb.init(MysqlDriver{},database_url.as_str()).unwrap();
 
 
-    let mut activity = Admin {
-        admin_id: Some(1.into()),
-        user_name: Some("用户名称".into()),
-        password:  Some("密码".into()),
-        salt:  Some("盐值".into()),
-        create_time:  Some(FastDateTime::now()),
-        nick_name: Some("昵称".into()),
-    };
+    // let mut activity = Admin {
+    //     admin_id: Some(1.into()),
+    //     user_name: Some("用户名称".into()),
+    //     password:  Some("密码".into()),
+    //     salt:  Some("盐值".into()),
+    //     create_time:  Some(DateTime::now()),
+    //     nick_name: Some("昵称".into()),
+    // };
     // let data = Admin::insert(&mut rb, &activity).await;
     // println!("insert = {:?}", data);
 
-    let data = Admin::list(&mut rb, "1").await;
+    let data = Admin::list(&mut rb, "3").await;
     println!("查询多条记录 = {:?}", data);
 
-    let data = Admin::one(&mut rb, "1").await;
+    let data = Admin::one(&mut rb, "3").await;
     println!("查询单条记录 = {:?}", data);
 
-    activity.user_name = Some(Uuid::new().to_string()[0..7].to_string());
-    let data = Admin::update(&mut rb, &activity, "1").await;
-    println!("修改 = {:?}", data);
+    // activity.user_name = Some(Uuid::new().to_string()[0..7].to_string());
+    // let data = Admin::update(&mut rb, &activity, "3").await;
+    // println!("修改 = {:?}", data);
 
-    // let data = Admin::delete(&mut rb, "2").await;
-    // println!("删除 = {:?}", data);
-
-
-    let data = Admin::page(&mut rb, &PageRequest::new(1, 10), "2").await;
+    let data = Admin::page(&mut rb, &PageRequest::new(1, 10)).await;
     println!("分页 = {:?}", data);
 }
--- rbatis/src/service/admin.rs
+++ rbatis/src/service/admin.rs
@@ -1,5 +1,5 @@
 use std::string::ToString;
-use rbatis::rbdc::datetime::FastDateTime;
+use rbatis::rbdc::datetime::{ DateTime};
 use serde::{Deserialize, Serialize};
 use rbatis::{impl_update, impl_delete, impl_select_page, impl_select};
 
@@ -11,14 +11,14 @@ pub struct  Admin {
     pub user_name: Option<String>,
     pub password: Option<String>,
     pub salt: Option<String>,
-    pub create_time: Option<FastDateTime>,
+    pub create_time: Option<DateTime>,
     pub nick_name: Option<String>,
 }
 
 rbatis::crud!(Admin {},TABLE_NAME);
 
-impl_select!(Admin{list(id:&str) => "`where admin_id = #{id}`"},TABLE_NAME);
-impl_select!(Admin{one(id:&str) -> Option => "`where admin_id = #{id} limit 1`"},TABLE_NAME);
-impl_update!(Admin{update(id:&str) => "`where admin_id = #{id}`"},TABLE_NAME);
-impl_delete!(Admin {delete(id:&str) => "`where admin_id = #{id}`"},TABLE_NAME);
-impl_select_page!(Admin{page(name:&str) => "`where user_name = #{name}`"},TABLE_NAME);
+impl_select!(Admin{list(id:&str) => "`where id = #{id}`"},TABLE_NAME);
+impl_select!(Admin{one(id:&str) -> Option => "`where id = #{id} limit 1`"},TABLE_NAME);
+impl_update!(Admin{update(id:&str) => "`where id = #{id}`"},TABLE_NAME);
+impl_delete!(Admin {delete(id:&str) => "`where id = #{id}`"},TABLE_NAME);
+impl_select_page!(Admin{page() => ""},TABLE_NAME);
